2c1f0e14550af610717d25880fbb32a18fe6fafe
Kevin <k.cardenas1795@gmail.com>
2024-03-07 23:29:52 -0500

    lll

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -2,38 +2,38 @@ import { useState } from "react";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-export default function Options({ setChooseTeam }) { 
+export default function Options({ setChooseTeam }) {
+    const navigate = useNavigate();
 
-    const navigate =  useNavigate()
-
-    
-    function handleFilter(e) { 
+    function handleFilter(e) {
         setChooseTeam(e.target.value);
-        navigate(`/quiz/${e.target.value}`)
-        
+        navigate(`/quiz/${e.target.value}`);
     }
-    
-        
-    return ( 
-            <div className="SearchBar">
-                
-                
 
-              <div className="buttonContainer">
-              <button value="Lakers" onClick={handleFilter}>Lakers</button>
-              <button value="TimberWolves" onClick={handleFilter}>TimberWolves</button>
-              <button value="OKC Thunder" onClick={handleFilter}>OKC Thunder</button>
-              <button value="Nuggets" onClick={handleFilter}>Nuggets</button>
-              <button value="Clippers" onClick={handleFilter}>Clippers</button>
+    return (
+        <div className="SearchBar">
+            <div className="buttonContainer">
+                <button className="teamButton" value="Lakers" onClick={handleFilter}>
+                    Lakers
+                </button>
+                <button className="teamButton" value="TimberWolves" onClick={handleFilter}>
+                    TimberWolves
+                </button>
+                <button className="teamButton" value="OKC Thunder" onClick={handleFilter}>
+                    OKC Thunder
+                </button>
+                <button className="teamButton" value="Nuggets" onClick={handleFilter}>
+                    Nuggets
+                </button>
+                <button className="teamButton" value="Clippers" onClick={handleFilter}>
+                    Clippers
+                </button>
             </div>
-            <input 
-                  
-                  type="text"
-                  placeholder="Search Teams"
-                  onChange={handleFilter}
-                  
-                  
-              />
-            </div>
-        );
+            <input
+                type="text"
+                placeholder="Search Teams"
+                onChange={handleFilter}
+            />
+        </div>
+    );
 }
