4d7046117c8bb0d656fd625977d1e82110c58b0f
cardenas1995 <154838641+cardenas1995@users.noreply.github.com>
2024-03-08 08:39:16 -0500

    Update MyPage.jsx

--- src/components/MyPage.jsx
+++ src/components/MyPage.jsx
@@ -1,3 +1,6 @@
+
+import React, { useState, useEffect } from "react";
+import Options from "./Options";
 import Home from "./Home";
 import { Outlet } from "react-router-dom";
 
@@ -24,32 +27,5 @@ export default function MyPage() {
             <Outlet context={{ filteredArray }} />
         </div>
     );
-}
-import Home from "./Home"; 
-import { Outlet } from "react-router-dom";
-
-export default function MyPage() { 
-    const [trivia, setTrivia] = useState([]);
-    const [chooseTeam, setChooseTeam] = useState("");
-
-    useEffect(() => { 
-        fetch('http://localhost:4000/trivia')
-            .then(res => res.json())
-            .then(triviaData => setTrivia(triviaData))
-    },[])
-
-    const filteredArray = trivia.filter(trivia => {
-        if(chooseTeam !== ''){
-            return trivia.team.toLowerCase().includes(chooseTeam.toLowerCase());
-        }
-    })
-    
-    return ( 
-        <div className="main-page">
-            <Home />
-            <Options setChooseTeam={setChooseTeam} />
-            {filteredArray.length > 0 && <Outlet context={{filteredArray}} />}
-        </div>
-    )
 }
 
