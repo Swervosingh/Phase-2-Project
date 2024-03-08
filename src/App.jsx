2202affb78d3cfcb31e1140321c33c5625189e0a
Kevin <k.cardenas1795@gmail.com>
2024-03-07 22:47:16 -0500

    lll

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+
 import MyPage from "./components/MyPage";
 
 export default function App() {
-    return (
-       <div>
+    return(
+        <div>
             <MyPage/>
         </div>
     );
