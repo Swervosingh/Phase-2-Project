14e501a3e0cfde93347742bc4062079c010356f5
Swervosingh <153397884+Swervosingh@users.noreply.github.com>
2024-03-07 20:06:27 -0500

    Added component About and Home

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const About = () => {
+  return (
+    <div>
+    <h1>🏀 Dive into the Heart of NBA Trivia Madness! 🏀</h1>
+    <p>Get ready to dribble through the ultimate hoops showdown! Test your NBA knowledge with our lightning-fast trivia challenges. From buzzer-beaters to iconic teams, dive into the heart of basketball history and compete for glory on our leaderboard. Think you have what it takes to beat the buzzer? Let the games begin!</p>
+    
+    </div>
+
+  )
+}
+
+export default About
--- src/components/ScorePage.jsx
+++ src/components/ScorePage.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-function ScorePage({ score }) {
-  return (
-    <div>
-      <h1>Quiz Completed!</h1>
-      <h2>Your Score: {score}</h2>
-      <Link to="/">Go back to homepage</Link>
-    </div>
-  );
+function ScorePage() {
+    const location = useLocation();
+    const score = location.state.score;
+
+    return (
+        <div>
+            <h1>Quiz Completed!</h1>
+            <h2>Your Score: {score}</h2>
+            <Link to="/">Go back to homepage</Link>
+        </div>
+    );
 }
 
 export default ScorePage;
